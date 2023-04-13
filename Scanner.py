ebbfcbcd6af34446cba779c952df4b3674b475e6
Matei-Stoian <stoian.matei782@gmail.com>
2023-04-13 23:01:59 +0300

    init

--- Scanner.py
+++ Scanner.py
@@ -2,36 +2,46 @@ import cv2
 import numpy as np
 import os
 import sys
+
 OUPUT_FOLDER = 'static/outputs'
+
 class Scanner:
     def __init__(self, path,filename):
         self.original = cv2.imread(path)
         self.scan()
         #self.display()
         self.save(filename)
+
     def biggets_countour(self,countours):
         bigget = np.array([])
+
         max_area = 0
         
         for cnt in countours:
             area = cv2.contourArea(cnt)
+
             if area > 1000:
                 peri = cv2.arcLength(cnt, closed=True)
                 approx = cv2.approxPolyDP(cnt, 0.015*peri, closed=True)
+
             if area > max_area and len(approx) == 4:
                 max_area = area
                 bigget = approx
+
         return bigget
+
     def display(self):
         cv2.imshow('',self.output)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
     def scan(self):
         gray = cv2.cvtColor(self.original,cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray,(5,5),0)
         canny = cv2.Canny(blur,50,250)
         conturs, _ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
         conturs = sorted(conturs,key=cv2.contourArea,reverse=True)[:10]
+
         big = self.biggets_countour(countours=conturs)
         points = big.reshape(4,2)
         input_arr = np.zeros((4,2),dtype="float32")
@@ -41,6 +51,7 @@ class Scanner:
         dpoints = np.diff(points,axis=1)
         input_arr[1] = points[np.argmin(dpoints)]
         input_arr[2] = points[np.argmax(dpoints)]
+
         (tl,tr,bl,br) = input_arr
         b_width = np.sqrt(((br[0]-bl[0]) ** 2) + ((br[1]-bl[1])**2))
         t_width = np.sqrt(((tr[0]-tl[0]) ** 2) + ((tr[1]-tl[1])**2))
@@ -51,5 +62,6 @@ class Scanner:
         converted_paints = np.float32([[0,0],[mwidth,0],[0,mheight],[mwidth,mheight]])
         matrix = cv2.getPerspectiveTransform(input_arr,converted_paints)
         self.output = cv2.warpPerspective(self.original,matrix,(mwidth,mheight))
+
     def save(self,filename):
         cv2.imwrite(os.path.join(OUPUT_FOLDER,filename),self.output)
--- server.py
+++ server.py
@@ -3,49 +3,60 @@ import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from Scanner import Scanner
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 UPLOAD_FOLDER = 'static/uploads/'
 SAVED_FOLDER = 'static/outputs/'
+
 for f in os.listdir(UPLOAD_FOLDER):
-    os.remove(os.path.join(UPLOAD_FOLDER,f))
+    os.remove(os.path.join(UPLOAD_FOLDER, f))
+
 for f in os.listdir(SAVED_FOLDER):
-    os.remove(os.path.join(SAVED_FOLDER,f))
+    os.remove(os.path.join(SAVED_FOLDER, f))
+
 app = Flask(__name__)
 app.secret_key = "secret_key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-app.config['SAVED_FOLDER']=SAVED_FOLDER
-
+app.config['SAVED_FOLDER'] = SAVED_FOLDER
 
 
 def allowed_file(filname):
-    return '.' in filname and filname.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filname and filname.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
 def upload_form():
     return render_template('upload_form.html')
 
+
 @app.route("/",  methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
         flash('No file part')
         return redirect(request.url)
+
     file = request.files['file']
+
     if file.headers == '':
         flash('No image selected for uploading')
         return redirect(request.url)
+
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-        Scanner(os.path.join(app.config['UPLOAD_FOLDER'],filename),filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        Scanner(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
         flash('Image successfully uploaded and displyed below')
-        return render_template('upload_form.html',filename=filename)
+        return render_template('upload_form.html', filename=filename)
     else:
         flash('Allowd image types are -> .png, .jpg, .jpeg')
         return redirect(request.url)
+
+
 @app.route('/display/<filename>')
 def display_image(filename):
-    return redirect(url_for('static',filename='outputs/'+filename),code=301)
+    return redirect(url_for('static', filename='outputs/'+filename), code=301)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
